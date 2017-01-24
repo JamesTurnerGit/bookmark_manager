dea6e88a959e613581de9e0ac7708a8b2841cffa
Matthew Perry <thesedatedprince@gmail.com>
2017-01-24 13:55:42 +0000

    Added link redirect

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ require_relative './app/models/link.rb'
 
 class Bookmark_manager < Sinatra::Base
 
+  get '/' do
+    redirect '/links'
+  end
+
   get '/links' do
     @links = Link.all
     erb(:links)
